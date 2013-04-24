d1a2901a950a9ea6f893fcb434bb36930182a9aa
yixiongz <yixiong.zhou@b-reel.com>
2013-04-24 17:38:27 -0400

    added 404 page under /php folder; added .htaccess that rewrites custom urls for the use of html5 history api

--- development/index.php
+++ development/index.php
@@ -13,16 +13,19 @@
 	  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, minimum-scale=1, maximum-scale=1">
 	  <meta name="keywords" content="">
 	  <meta name="description" content="">
-	  <link rel="shortcut icon" href="img/favicon.ico">
-	  <link rel="stylesheet" href="css/main.css" media="screen">
-
-		<script src="js/third-party/greensock/TweenMax.min.js"></script>
-
-		<!--<script src="js/compiled.js"></script>-->
-		<script src="http://localhost:9810/compile?id=test&mode=<?php echo $COMPILE_MODE ?>"></script>
+	  <link rel="shortcut icon" href="/img/favicon.ico">
+	  <link rel="stylesheet" href="/css/main.css" media="screen">
 	</head>
 
 	<body>
+
+
+		<!-- third-party -->
+		<script src="/js/third-party/greensock/TweenMax.min.js"></script>
+
+		<!-- project js -->
+		<!--<script src="js/compiled.js"></script>-->
+		<script src="http://localhost:9810/compile?id=test&mode=<?php echo $COMPILE_MODE ?>"></script>
 
 		<!-- execute the main js-->
 		<script>
